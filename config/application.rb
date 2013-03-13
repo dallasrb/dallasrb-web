924daf5921ed31ec701dc3a0defd15999c9cd9b9
Karthik Hariharan <kar.hariharan@gmail.com>
2013-03-12 23:34:31 -0500

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ end
 
 module DallasrbWeb
   class Application < Rails::Application
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec
+      
+      
+      
+      
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
