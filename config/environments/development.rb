d3ecc298b76d3a1e7607be93319f1b70540f000a
Karthik Hariharan <kar.hariharan@gmail.com>
2013-03-12 23:34:31 -0500

    rails_apps_composer: set email accounts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,24 @@ DallasrbWeb::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 25,
+    domain: "example.com",
+    authentication: "plain",
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
+  }
+
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
