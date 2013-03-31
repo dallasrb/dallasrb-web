8a4fcd0b3a1c24af0b13c3ceca0e2027efac5e5a
Karthik Hariharan <kar.hariharan@gmail.com>
2013-03-30 23:24:11 -0500

    Fixing format for event times.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -13,4 +13,13 @@ module EventsHelper
       marker_html.html_safe
     end
   end
+
+  def event_timestamp(event)
+    event_date = event.event_date
+    start_time = event_date.strftime("%l%p")
+    end_time = (event_date + 2.hours).strftime("%l%p")
+    date = event.event_date.strftime(" on %A, %B #{event_date.day.ordinalize}, %Y")
+
+    "#{start_time} to #{end_time} on #{date}"
+  end
 end
