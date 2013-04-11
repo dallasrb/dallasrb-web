545ff707c43ae2a170a80723e42598bce8d3fc56
Karthik Hariharan <kar.hariharan@gmail.com>
2013-04-10 23:15:45 -0500

    Fixing secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DallasrbWeb::Application.config.secret_token = '3381ba21c3cac408db22013007692c1944060ee4032d694ba77e6587c4b96a68cb0a3cfacb9cac1d3e078355b473821739bebc465c00a12591b2842396caab3d'
+DallasrbWeb::Application.config.secret_token = ENV['SECRET_TOKEN']
