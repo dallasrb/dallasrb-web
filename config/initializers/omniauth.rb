5f1d20c5e971627dbc0b8d167863f2bced5a9bcc
Karthik Hariharan <kar.hariharan@gmail.com>
2013-05-02 20:49:38 -0500

    Fixing GitHub omniauth to allow reading user emails.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], scope: "user:email"
 end
