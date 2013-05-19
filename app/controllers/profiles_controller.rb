073fc4a255325c062b6db386bafefd036227f906
Manu S Ajith <neo@codingarena.in>
2013-05-20 01:37:04 +0530

    Fixed bug with show action in profile and added action for searching profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,25 @@
 class ProfilesController < ApplicationController
   #user must be signed in to edit the profile
-  before_filter :authenticate_user!, except:[:index]
-  
+  autocomplete :profile, :name
+  before_filter :authenticate_user!, only:[:edit,:update]
+
+
   def index
     @profiles = Profile.non_organizers.order('sort_order asc').page(params[:page]).per(8)
+  end
+
+  def show
+    @profile = Profile.non_organizers.find(params[:id])
+  end
+
+  def search
+    begin
+      @profile = Profile.non_organizers.find(params[:profile][:id])
+      render :show
+    rescue
+      flash[:notice] = "Searched Profile not found"
+      redirect_to profiles_path
+    end
   end
 
   def edit
